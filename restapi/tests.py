6e7cb43768f8605f86bc050feb37ca468d6077d9
divyachandrapandi <divya4shivalaya@gmail.com>
2022-03-12 07:04:27 +0530

    Adding first python test and hgithub actions

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
+
 
 # Create your tests here.
+def sum_of_integers(a, b):
+    return a + b
+
+
+class Testsum(TestCase):
+    def test_sum(self):
+        self.assertEqual(sum_of_integers(3, 2), 5)
